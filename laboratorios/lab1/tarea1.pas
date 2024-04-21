26676d493f8ca2e79480e4d7f154e05f43c783f6
guillewaj <74830588+GWOZZ@users.noreply.github.com>
2024-04-21 05:33:17 -0300

    lab1

--- laboratorios/lab1/tarea1.pas
+++ laboratorios/lab1/tarea1.pas
@@ -1,11 +1,34 @@
-program AnalisisOracion(input, output);
+procedure leerPalabraLargo (var largo : integer; var fin : boolean);
 var
-
-procedure leerPalabraLargo ( var largo : integer
-; var fin : boolean);
-
-procedure leerOracionDatos (var cantPalabras,
-masLarga, masCorta : integer);
-
+    caracter :  char;
 begin
-end.
+    fin := false;
+    largo := 0;
+    read(caracter);
+    repeat
+        read(caracter);
+        largo := largo + 1
+    until (caracter = SEPARADOR) or (caracter = FINALIZADOR);
+    if caracter = FINALIZADOR then
+        fin := true
+end;
+
+procedure leerOracionDatos (var cantPalabras, masLarga, masCorta : integer);
+var
+    max, min, largo : integer;
+    fin : boolean;
+begin
+    fin := false;
+    leerPalabraLargo(largo , fin);
+    max := largo;
+    min := largo;
+    cantPalabras := 1;
+    while fin = false do begin
+        leerPalabraLargo(largo , fin);
+        cantPalabras := cantPalabras + 1;
+        if largo < min then min := largo
+        else if largo > max then max := largo
+    end;
+    masCorta := min;
+    masLarga := max
+end;
