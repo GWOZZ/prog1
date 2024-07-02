919ae0373aa4c2a37488d3eea38677470d5b3b92
guillewaj <74830588+GWOZZ@users.noreply.github.com>
2024-07-02 06:25:00 -0300

    l2

--- laboratorios/lab2/Test2/tarea2.pas
+++ laboratorios/lab2/Test2/tarea2.pas
@@ -91,7 +91,7 @@ begin
         if not pc.esColumna then 
             txt := txt^.sig
     end;
-    pp.esPosicion := (txt <> nil);
+    pp.esPosicion := pc.esColumna;
     if pp.esPosicion then begin
         pp.p.linea := contador;
         pp.p.columna := pc.col
@@ -106,28 +106,72 @@ procedure insertarCadenaEnLinea (c : Cadena; columna : RangoColumna; var ln : li
     Si (c.tope + ln.tope) supera `MAXCOL`, los carácteres sobrantes se retornan (en
     orden) en la posible línea `pln`.
  
-    Precondiciones:  1 <= columna <= ln.tope+1
-                   columna <= MAXCOL
-                   c.tope + columna <= MAXCOL   }  
+    Precondiciones: 1 <= columna <= ln.tope+1
+                    columna <= MAXCOL
+                    c.tope + columna <= MAXCOL   }  
+{type
+    LineaExtendida = record  
+		cars : array [1..(MAXCOL+MAXCAD)] of Caracter;
+		tope : 0..(MAXCOL+MAXCAD)
+	end;	} 
 var
+    {lntmp : LineaExtendida;}
+    nuevoformato : Formato;
     tfmt : TipoFormato;
     i, j : integer;
 begin
     pln.esLinea := (c.tope + ln.tope > MAXCOL);
     if pln.esLinea then begin
         pln.l.tope := ln.tope + c.tope - MAXCOL;
-        j := 0;
-        for i := ln.tope downto columna do begin
-            pln.l.cars[pln.l.tope - j] := ln.cars[i];
-            j := j + 1
-        end;
-        for i := (pln.l.tope - j) downto 1 do begin
-            pln.l.cars[i].car := c.cars[c.tope - i + 1];
-            pln.l.cars[i].fmt := ln.cars[columna].fmt
-        end;
-        for i := 1 to (c.tope - j) do begin
-            ln.cars[columna + i - 1].car := c.cars[i];
-            ln.cars[columna + i - 1].fmt := ln.cars[columna].fmt
+        if columna = ln.tope + 1 then begin
+            for tfmt := Neg to Sub do
+                nuevoformato[tfmt] := false;
+            i := 1;
+            while ln.tope < MAXCOL do begin
+                ln.tope := ln.tope + 1;
+                ln.cars[ln.tope].car := c.cars[i];
+                ln.cars[ln.tope].fmt := nuevoformato;
+                i := i + 1;
+            end;
+            j := 1;
+            for i := i to c.tope do begin
+                pln.l.cars[j].car := c.cars[i];
+                pln.l.cars[j].fmt := nuevoformato;
+                j := j + 1
+            end;
+        end
+        else begin
+            nuevoformato := ln.cars[columna].fmt;
+            i := pln.l.tope;
+            j := ln.tope;
+            while (j >= columna) and (i >= 1) do begin
+                pln.l.cars[i] := ln.cars[j];
+                i := i - 1;
+                j := j - 1
+            end;
+            ln.tope := MAXCOL;
+            if i >= 1 then begin
+                for j := 1 to c.tope - i do begin
+                    ln.cars[columna].car := c.cars[j];
+                    ln.cars[columna].fmt := nuevoformato;
+                    columna := columna + 1;
+                end;
+                for j := c.tope - i to c.tope do begin
+                    pln.l.cars[j - (c.tope - i) + 1].car := c.cars[j];
+                    pln.l.cars[j - (c.tope - i) + 1].fmt := nuevoformato
+                end;
+            end
+            else begin
+                i := MAXCOL;
+                for j := j downto columna do begin
+                    ln.cars[i] := ln.cars[j];
+                    i := i - 1
+                end;
+                for i := 1 to c.tope do begin
+                    ln.cars[columna + i - 1].car := c.cars[i];
+                    ln.cars[columna + i - 1].fmt := nuevoformato
+                end
+            end
         end
     end
     else begin
@@ -146,6 +190,38 @@ begin
         ln.tope := ln.tope + c.tope
     end
 end;
+{begin
+    if columna = ln.tope + 1 then
+        for tfmt := Neg to Sub do
+            nuevoformato[tfmt] := false
+    else
+        nuevoformato := ln.cars[columna].fmt;
+    lntmp.tope := c.tope + (ln.tope - (columna - 1));
+    for i := 1 to c.tope do begin
+        lntmp.cars[i].car := c.cars[i];
+        lntmp.cars[i].fmt := nuevoformato
+    end;
+    j := 1;
+    for i := columna to ln.tope do begin
+        lntmp.cars[c.tope + j] := ln.cars[i];
+        j := j + 1
+    end;
+    i := 1;
+    j := columna;
+    while (j <= MAXCOL) and (i <= lntmp.tope) do begin
+        ln.cars[j] := lntmp.cars[i];
+        j := j + 1;
+        i := i + 1
+    end;
+    j := 1;
+    for i := i to lntmp.tope do begin
+        pln.l.cars[j] := lntmp.cars[i];
+        j := j + 1
+    end;
+    pln.esLinea := ln.tope + c.tope > MAXCOL;
+    if pln.esLinea then
+        pln.l.tope := ln.tope + c.tope - MAXCOL 
+end;}
 
 procedure insertarLineaEnTexto (ln : Linea; nln : integer; var txt : Texto);
 {   Inserta la línea `ln` en la posición `nln` del texto `txt`.
