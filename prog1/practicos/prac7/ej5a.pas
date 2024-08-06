cfd09a5ba7e17d50f88cc10f19c60bd4d9733786
GWOZZ <guillermowajner@gmail.com>
2024-08-06 01:21:14 -0300

    mergee

--- prog1/practicos/prac7/ej5a.pas
+++
@@ -1,9 +0,0 @@
-procedure corrimiento(var a, b, c: integer);
-var
-    aux : integer;
-begin
-    aux := a;
-    a := c;
-    c := b;
-    b := aux;    
-end;
--- prog1/practicos/prac7/ej9a.pas
+++
@@ -1,8 +0,0 @@
-function multiplo(m, n : integer) : boolean;
-{ pre: m, n > 0 }
-begin
-    if (n mod m = 0) or (m mod n = 0) then
-        multiplo := true
-    else
-        multiplo := false
-end;
